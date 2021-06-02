ef6bc2d825cecb4e6390347187e7b36010765fa9
msorozabal <82554154+msorozabal@users.noreply.github.com>
2021-06-02 16:56:18 -0300

    Add files via upload

---
+++ 05_anagram.R
@@ -0,0 +1,32 @@
+library(testthat)
+
+context("anagram")
+
+anagram <- function(word,anagrams) {
+  lista.anagrams=list()
+  for(i in anagrams){
+      anagram.split=unlist(strsplit(i,""))
+      word.split=unlist(strsplit(word,""))
+      anagram.sort=sort(anagram.split)
+      word.sort=sort(word.split)
+      anagram.sort.lower=tolower(anagram.sort)
+      word.sort.lower=tolower(word.sort)
+      lower.word=tolower(word)
+      lower.anagram=tolower(i)
+      if (!(identical(lower.word,lower.anagram))){
+      if (identical(anagram.sort.lower,word.sort.lower)){
+        lista.anagrams <- list(unlist(c(lista.anagrams, i),recursive=FALSE))
+        print(lista.anagrams)
+      }
+      }
+    }
+}
+
+test_that("is anagram", {
+  subject <- "samymassitas"
+  candidates <- c("massitassamy","nosamy","samymassisat")
+  expect_equal(anagram(subject, candidates),
+               c())
+})
+
+message("passed: anagram")
